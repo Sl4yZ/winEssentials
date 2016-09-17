34c8758f669ee5551bf53356baf0d149b11b9c14
Sl4yZ <rxzeditor@gmail.com>
2016-09-17 14:38:37 +0200

    Rajout de tout

---
+++ winEssentials/winEssentials/CommandH.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winEssentials
+{
+    public class CommandH
+    {
+        public static void Redirect(string command)
+        {
+            string[] commandSplitted = command.Split(' ');
+
+            if (commandSplitted.Length >= 1)
+            {
+                if (commandSplitted[0] == "decrash") // decrash | length = 1
+                {
+                    Console.WriteLine("\\ Affichage des processus ne répondant pas //");
+                    Console.WriteLine("----------------------------------------------");
+
+                    List<ProcessManager.structProcess> processNotResponding = ProcessManager.getProcessNotResponding();
+
+                    foreach (ProcessManager.structProcess sp in processNotResponding)
+                    {
+                        Console.WriteLine("NAME : " + sp.name + " ID : " + sp.id);
+                    }
+
+                    Console.WriteLine("----------------------------------------------");
+                    bool finish = false;
+
+                    while (!finish)
+                    {
+                        Console.WriteLine("' kill [id] ' kill the process with the id or ' kill ' kill all the process which don't responding");
+
+                        string commandKill = Console.ReadLine();
+                        string[] commandKillSplitted = commandKill.Split(' ');
+
+                        if (commandKillSplitted.Length == 1)
+                        {
+                            //Kill tout les processus
+                            finish = true;
+                        }
+                        else if (commandKillSplitted.Length == 2)
+                        {
+                            //Kill le processus avec l'id
+                            finish = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ winEssentials/winEssentials/ProcessManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace winEssentials
+{
+    class ProcessManager
+    {
+        public struct structProcess
+        {
+            public string name;
+            public int id;
+        }
+
+        public static List<structProcess> getProcessNotResponding()
+        {
+            List<structProcess> processNotResponding = new List<structProcess>();
+
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist)
+            {
+                if (!theprocess.Responding)
+                {
+                    structProcess spToAdd = new structProcess();
+                    spToAdd.name = theprocess.ProcessName;
+                    spToAdd.id = theprocess.Id;
+
+                    processNotResponding.Add(spToAdd);
+                }
+            }
+
+            return processNotResponding;
+        }
+
+        public static bool killProcessByID(int id)
+        {
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist)
+            {
+                if (theprocess.Id == id)
+                {
+                    theprocess.Kill();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool killAllProcess()
+        {
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist)
+            {
+                if (!theprocess.Responding)
+                {
+                    theprocess.Kill();
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
