8c21863eb642798a4418cdaef59da278f69cc5f3
Sl4yZ <rxzeditor@gmail.com>
2016-09-29 22:15:01 +0200

    ram cpu

--- winEssentials/winEssentials/Program.cs
+++ winEssentials/winEssentials/Program.cs
@@ -1,18 +1,23 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace winEssentials
 {
     class Program
     {
+
+        private static System.Windows.Forms.Timer timer;
         [STAThread]
         static void Main(string[] args)
         {
+          
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine(".: winEssential v1 - Beta version :.");
             Console.ForegroundColor = ConsoleColor.DarkYellow;
@@ -21,6 +26,9 @@ namespace winEssentials
             Console.WriteLine("Type /help for list of commands available or visit our website : winEssential.tk");
             Console.WriteLine(" ");
             Console.ResetColor();
+            Timer t = new Timer(updateTimer_Tick, null, 0, 1000);
+            InitialiseCPUCounter();
+
 
             while (true)
             {
@@ -30,6 +38,34 @@ namespace winEssentials
 
             
         }
-        
+        static void updateTimer_Tick(Object o)
+        {
+            Console.Title = "winEssentials | CPU Usage: " +
+            InitialiseCPUCounter();
+          
+
+            Console.Title += " RAM Usage: " + InitializeRAMCounter();
+        }
+
+
+        public static string InitialiseCPUCounter()
+        {
+             PerformanceCounter cpuCounter;
+            cpuCounter = new PerformanceCounter();
+
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+            return cpuCounter.NextValue() + "% |";
+        }
+
+        public static string InitializeRAMCounter()
+        {
+            PerformanceCounter ramCounter;
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            return ramCounter.NextValue() + " MB";
+
+        }
+
     }
 }
